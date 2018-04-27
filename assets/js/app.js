3f4200b7eefe2000cdc200d6feca4f094591ccae
Balthasar <rileysprice@gmail.com>
2018-04-27 15:09:23 -0600

    ARGH

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,18 @@
 const baseUrl = 'https://swapi.co/api/people/'
 const characterSection = document.querySelector('#partner1')
-const roll = math.random()
+const characterSection2 = document.querySelector('#partner2')
+
+// const random = (Math.random() * 100).toFixed(0)
 
 function getCharacters() {
 
-    fetch(baseUrl + 'roll')
+    fetch(baseUrl + (Math.random() * 88).toFixed(0) + '/')
         .then(function (response) {
             return response.json()
         })
         .then(function (people) {
-            console.log(people)
             var peopleArray = people.results
-            for (var i = 0; i < 1; i++) {
+            for (var i = 0; i < peopleArray.length; i++) {
                 var person = people.results[i]
                 var name = person.name
                 var hairColor = person.hair_color
@@ -33,3 +34,35 @@ function getCharacters() {
         })
 }
 getCharacters();
+
+// function getPartner2() {
+
+//     fetch(baseUrl + (Math.random() * 88).toFixed(0) + '/')
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (people) {
+//             console.log(people)
+//             var peopleArray = people.results
+//             for (var i = 0; i < 1; i++) {
+//                 var person = people.results[i]
+//                 var name = person.name
+//                 var hairColor = person.hair_color
+//                 var height = person.height
+//                 var gender = person.gender
+//                 var html = `
+//           <section>
+//             <h2>${name}</h2>
+//             <ul>
+//               <li>Hair: ${hairColor}</li>
+//               <li>Height: ${height}cm</li>
+//               <li>Gender: ${gender}</li>
+//             </ul>
+//           </section>
+//         `
+//                 partner2.innerHTML += html
+
+//             }
+//         })
+// }
+// getPartner2();
